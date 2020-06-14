56ec2f025eb332756dc05c2310701e5d4eba3d83
dargor980 <german.contrerasa@utem.cl>
2020-06-13 23:52:17 -0400

    se hizo el login docentes

---
+++ Sistema_docentes/log_docente.php
@@ -0,0 +1,27 @@
+<?php
+
+    session_start();
+    $email= $_POST['user'];
+    $password= $_POST['password'];
+    //sql conection
+
+    $servername= "localhost";
+    $userDatabase ="root";
+    $passwordDatabase ="";
+    $database= "UNIVERSIDAD";
+
+    $con = mysqli_connect($servername,$userDatabase,$passwordDatabase,$database) or
+        die("Error en la conexión a la base de datos");
+
+    $registro = mysqli_query($con, "SELECT *FROM credencial WHERE ") or
+        die("Query error: " .mysqli_error($con));
+
+    if($reg=mysqli_fetch_array($registro)){
+        $_SESSION['email']= $email;
+        header("Location: ");
+    }
+    else{
+        echo "El email o password es incorrecto";
+    }
+
+?>
